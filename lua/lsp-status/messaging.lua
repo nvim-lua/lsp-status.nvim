527dfd319c07c47e4537fc532966f445802a33a7
Wil Thomason <wil.thomason@gmail.com>
2020-06-30 19:26:10 -0400

    fix: gracefully handle clients that shut down with remaining messages

--- lua/lsp-status/messaging.lua
+++ lua/lsp-status/messaging.lua
@@ -2,8 +2,12 @@ local util = require('lsp-status/util')
 
 local clients = {}
 local messages = {}
-local function init(_messages, _)
-  messages = _messages
+local function init(_messages, _) messages = _messages end
+
+-- Unregister stopped clients
+local function unregister_client(id)
+  messages[id] = nil
+  clients[id] = nil
 end
 
 local function progress_callback(_, _, msg, client_id)
@@ -15,18 +19,19 @@ local function progress_callback(_, _, msg, client_id)
         title = val.title,
         message = val.message,
         percentage = val.percentage,
-        spinner = 1,
+        spinner = 1
       }
     elseif val.kind == 'report' then
       messages[client_id].progress[msg.token].message = val.message
       messages[client_id].progress[msg.token].percentage = val.percentage
-      messages[client_id].progress[msg.token].spinner = messages[client_id].progress[msg.token].spinner + 1
+      messages[client_id].progress[msg.token].spinner =
+        messages[client_id].progress[msg.token].spinner + 1
     elseif val.kind == 'end' then
       if messages[client_id].progress[msg.token] == nil then
         vim.api.nvim_command('echohl WarningMsg')
         vim.api.nvim_command(
-          'echom "[lsp-status] Received `end` message with no corresponding `begin` from  ' .. clients[client_id] .. '!"'
-        )
+          'echom "[lsp-status] Received `end` message with no corresponding `begin` from  '
+            .. clients[client_id] .. '!"')
         vim.api.nvim_command('echohl None')
       else
         messages[client_id].progress[msg.token].message = val.message
@@ -35,7 +40,7 @@ local function progress_callback(_, _, msg, client_id)
       end
     end
   else
-    table.insert(messages[client_id], { content = val, show_once = true, shown = 0 })
+    table.insert(messages[client_id], {content = val, show_once = true, shown = 0})
   end
 
   vim.api.nvim_command('doautocmd User LspMessageUpdate')
@@ -47,56 +52,50 @@ local function get_messages()
   local msg_remove = {}
   local progress_remove = {}
   for client, data in pairs(messages) do
-    for token, ctx in pairs(data.progress) do
-      table.insert(new_messages, {
-        name = data.name,
-        title = ctx.title,
-        message = ctx.message,
-        percentage = ctx.percentage,
-        progress = true,
-        spinner = ctx.spinner,
-      })
+    if vim.lsp.client_is_stopped(client) then
+      unregister_client(client)
+    else
+      for token, ctx in pairs(data.progress) do
+        table.insert(new_messages, {
+          name = data.name,
+          title = ctx.title,
+          message = ctx.message,
+          percentage = ctx.percentage,
+          progress = true,
+          spinner = ctx.spinner
+        })
 
-      if ctx.done then
-        table.insert(progress_remove, { client = client, token = token })
+        if ctx.done then table.insert(progress_remove, {client = client, token = token}) end
       end
-    end
 
-    for i, msg in ipairs(data.messages) do
-      if msg.show_once then
-        msg.shown = msg.shown + 1
-        if msg.shown > 1 then
-          table.insert(msg_remove, { client = client, idx = i })
+      for i, msg in ipairs(data.messages) do
+        if msg.show_once then
+          msg.shown = msg.shown + 1
+          if msg.shown > 1 then table.insert(msg_remove, {client = client, idx = i}) end
         end
+
+        table.insert(new_messages, {name = data.name, content = msg.content})
       end
 
-      table.insert(new_messages, { name = data.name, content = msg.content })
-    end
-
-    if next(data.status) ~= nil then
-      table.insert(new_messages, {
-        name = data.name,
-        content = data.status.content,
-        uri = data.status.uri,
-        status = true
-      })
+      if next(data.status) ~= nil then
+        table.insert(new_messages, {
+          name = data.name,
+          content = data.status.content,
+          uri = data.status.uri,
+          status = true
+        })
+      end
     end
   end
 
-  for _, item in ipairs(msg_remove) do
-    table.remove(messages[item.client].messages, item.idx)
-  end
+  for _, item in ipairs(msg_remove) do table.remove(messages[item.client].messages, item.idx) end
 
-  for _, item in ipairs(progress_remove) do
-    messages[item.client].progress[item.token] = nil
-  end
+  for _, item in ipairs(progress_remove) do messages[item.client].progress[item.token] = nil end
 
   return new_messages
 end
 
-local function register_progress()
-  vim.lsp.callbacks['$/progress'] = progress_callback
-end
+local function register_progress() vim.lsp.callbacks['$/progress'] = progress_callback end
 
 -- Client registration for messages
 local function register_client(id, name)
@@ -114,7 +113,7 @@ local M = {
   register_progress = register_progress,
   register_client = register_client,
   _init = init,
-  capabilities = capabilities,
+  capabilities = capabilities
 }
 
 return M

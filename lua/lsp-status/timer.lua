22700ccb6461497a6ef048943481281e4b46b0c5
Joshua Chapman <joshua.samuel.chapman@protonmail.com>
2021-03-22 17:05:53 +0100

    Fix missing status update
    This will redraw one more time after the last LSP update

--- lua/lsp-status/timer.lua
+++ lua/lsp-status/timer.lua
@@ -1,18 +1,28 @@
 --- Timer instance
 local timer = nil
+--- This flag is used to redraw one more time after the las LSP update.
+-- NOTE: This is achieved by setting the redraw_flag to true if
+-- vim.b.lsp_status_redraw is true and there has been an update. Next time if
+-- vim.b.lsp_status_redraw is false (no update) it will still redraw the
+-- statusline and this flag will be set to false.
+local redraw_flag = false
 
 --- Timer callback function
 -- NOTE: This function uses get_lsp_statusline so that needs nvim api functions
 -- so it should call in the vim.schedule_wrap()
 local function timer_callback()
   -- Check if need to redraw
-  if vim.b.lsp_status_redraw then
+  local update_flag = vim.b.lsp_status_redraw
+  if update_flag or redraw_flag then
     vim.b.lsp_status_redraw = false
     -- Schedule the command when it's safe to call it
     local new_state = require('lsp-status/statusline').get_lsp_statusline()
     if new_state ~= vim.b.lsp_status_statusline then
       vim.b.lsp_status_statusline = new_state
       vim.api.nvim_command('redrawstatus!')
+      -- If this was an lsp update (update_flag is true) redraw also next time
+      -- by setting redraw_flag
+      redraw_flag = update_flag
     end
   end
 end

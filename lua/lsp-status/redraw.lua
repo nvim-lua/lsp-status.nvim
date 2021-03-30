bc89089e32b137c294376462b12acb8b2193e98d
Wil Thomason <wil.thomason@gmail.com>
2021-03-30 01:46:13 -0400

    Fix missing case for setting timer_going

--- lua/lsp-status/redraw.lua
+++ lua/lsp-status/redraw.lua
@@ -2,15 +2,16 @@ local config = {}
 local function init(_, _config) config = vim.tbl_extend('force', config, _config) end
 
 local last_redraw = nil
+local timer_going = false
 
 local function redraw_callback(now)
   last_redraw = now or vim.loop.now()
   vim.api.nvim_command('redrawstatus!')
+  timer_going = false
 end
 
 local wrapped_redraw_callback = vim.schedule_wrap(redraw_callback)
 local timer = vim.loop.new_timer()
-local timer_going = false
 local function redraw()
   local now = vim.loop.now()
   if last_redraw == nil or now - last_redraw >= config.update_interval then
